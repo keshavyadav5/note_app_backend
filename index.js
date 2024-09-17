29acd821003cc2b2d1950ce05b44191bf0e82102
Keshav Yadav <kesavyadav992@gmail.com>
2024-09-18 01:05:38 +0530

    add reder backend link to app.cors to set up front workable

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://keshavnoteapp.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
