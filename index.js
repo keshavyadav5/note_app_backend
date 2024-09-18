7900f50300a9fca4c3751263327678d1cf8de8ec
Keshav Yadav <kesavyadav992@gmail.com>
2024-09-18 17:43:52 +0530

    change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://keshavnoteapp.netlify.app',
+  origin: 'http://localhost:5173/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
